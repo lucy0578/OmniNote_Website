8d3ee9c2efb050dc740416469790510728e9ae62
lucy0578 <1196925023@qq.com>
2025-03-10 22:10:01 +0800

    增加验证和邮箱

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -6,21 +6,27 @@
         registerForm: {
           username: '',
           password: '',
+          email: ''
         }
       }
     },
     methods: {
       onSubmit(){
-        // this.axios.post("/register", {
-        //   username: this.registerForm.username,
-        //   password: this.registerForm.password
-        // })
-        // .then(function (response) {
-        //   console.log(response)
-        // })
-        // .catch(function (error) {
-        //   console.log(error)
-        // })
+        if (this.registerForm.password !== this.registerForm.confirm) {
+          alert("Password and confirmation password do not match!");
+          return;
+        }
+        this.axios.post("/register", {
+          username: this.registerForm.username,
+          password: this.registerForm.password,
+          email: this.registerForm.email
+        })
+        .then(function (response) {
+          console.log(response)
+        })
+        .catch(function (error) {
+          console.log(error)
+        })
       }
     }
   }
@@ -30,13 +36,19 @@
   <div>
     <el-form ref="form" :model="registerForm" label-width="80px">
       <el-form-item>
-        <el-input placeholder="请输入用户名" v-model="registerForm.username"></el-input>
+        <el-input placeholder="Please enter your username" v-model="registerForm.username"></el-input>
       </el-form-item>
       <el-form-item>
-        <el-input placeholder="请输入密码" v-model="registerForm.password" show-password></el-input>
+        <el-input placeholder="Please enter your password" v-model="registerForm.password" show-password></el-input>
       </el-form-item>
       <el-form-item>
-        <el-button type="primary" @click="onSubmit" class="form-confirm">注册</el-button>
+        <el-input placeholder="Please confirm your password" v-model="registerForm.confirm" show-password></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-input placeholder="Please enter your email" v-model="registerForm.email"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit" class="form-confirm">Register</el-button>
       </el-form-item>
     </el-form>
   </div>
